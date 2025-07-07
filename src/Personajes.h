cb6dd1331b3d098a3a9aab9e4e1e9232ebb32aa8
Daniel Hernández <00036225@uca.edu.sv>
2025-07-07 09:56:34 -0600

    Update Personajes.h

---
+++ src/Personajes.h
@@ -0,0 +1,16 @@
+#ifndef PERSONAJES_H
+#define PERSONAJES_H
+
+#include <string>
+#include <vector>
+
+struct Personaje {
+    std::string nombre;
+    int hp, maxHp;
+    int atk, def;
+    std::vector<std::string> habilidades;
+};
+
+std::vector<Personaje> crearEquipo();
+
+#endif
